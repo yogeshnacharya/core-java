31a94572abe41a2d649a5994089581590b2f1208
yogeshnacharya <yn85980@gmail.com>
2026-03-29 17:49:26 +0530

    9-3-26 array task

---
+++ TraineeAge.java
@@ -0,0 +1,14 @@
+class TraineeAge {
+	public static void main(String[] age)
+	
+	{
+		int[] ages={20,21,22,23,24,25,26};
+		System.out.println(ages[0]);
+		System.out.println(ages[1]);
+		System.out.println(ages[2]);
+		System.out.println(ages[3]);
+		System.out.println(ages[4]);
+		System.out.println(ages[5]);
+		System.out.println(ages[6]);
+	}
+}
---
+++ TraineeName.java
@@ -0,0 +1,10 @@
+class TraineeName {
+public static void main(String [] args)
+{
+String [] traineeNames={"Amrutha","Punith","Zakeer Hussain","Manya","Rajesh"};
+System.out.println("Trainee Name:"+traineeNames[0]);
+System.out.println("Trainee Name:"+traineeNames[3]);
+System.out.println("Trainee Name:"+traineeNames[1]);
+System.out.println("Trainee Name:"+traineeNames[4]);
+System.out.println("Trainee Name:"+traineeNames[2]);
+}}
