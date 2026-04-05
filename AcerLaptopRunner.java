6aaf92204f08629d31881d79572e40c9bd72553e
yogeshnacharya <yn85980@gmail.com>
2026-04-05 12:37:44 +0530

    deleted

--- AcerLaptopRunner.java
+++
@@ -1,8 +0,0 @@
-class AcerLaptopRunner {
-public static void main(String[] acer)
-{
-AcerLaptop.getAcerLaptopBrandDetails();
-AcerLaptop.getAcerLaptopDetails("256 GB","Aspire 5 A515-58M",15.8f,"Steel Gray","Intel Core i5-1335U");
-AcerLaptop.getAcerLaptopPriceDetails(20000);
-}
-}
