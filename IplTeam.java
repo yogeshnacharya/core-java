b9b8795fe6bb72b67f716059a6e4841661adc33c
yogeshnacharya <yn85980@gmail.com>
2026-04-05 11:36:46 +0530

    initializing variables using constructor chaining

--- IplTeam.java
+++ IplTeam.java
@@ -1,32 +1,29 @@
 class IplTeam
 {
-	String teamName;
-	String captainName;
-	static int noOfPlayers=11;
-	String coachName;
-	String ownerName;
-	String jerseyColour;
-	
-	public IplTeam()
-	{
-		
-	}
-	public IplTeam(String teamName,String captainName,String coachName,String ownerName,String jerseyColour)
-	{
-		this.teamName=teamName;
-		this.captainName=captainName;
-		this.coachName=coachName;
-		this.ownerName=ownerName;
-		this.jerseyColour=jerseyColour;
-	}
-	public  void displayIplTeamDetails()
-	{
-		System.out.println("Team Name:"+teamName);
-		System.out.println("Captain Name:"+captainName);
-		System.out.println("No Of Players:"+noOfPlayers);
-		System.out.println("Coach Name:"+coachName);
-		System.out.println("Owner Name:"+ownerName);
-		System.out.println("Jersey Colour:"+jerseyColour);
-		System.out.println("---------------------------");
-	}
+ String teamName;
+ static int noOfPlayers=11;
+ String ownerName;
+ String captainName;
+
+ public IplTeam()
+ {
+	System.out.println("Running no argument constructors in ipl team");
+ }
+  
+ public IplTeam(String teamName,String ownerName,String captainName)
+ {
+  this.teamName=teamName;
+  this.ownerName=ownerName;
+  this.captainName=captainName;
+ }
+
+ public void displayIplTeamDetails()
+ {
+  System.out.println("Team Name:"+teamName);
+  System.out.println("Owner Name:"+ownerName);
+  System.out.println("No of Players:"+noOfPlayers);
+  System.out.println("Captain Name:"+captainName);
+  }
+
 }
+
